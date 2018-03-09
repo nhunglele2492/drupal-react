996a6021517ae1cbee1610904822facd111305c4
nhunglele2492 <nhung.le@ffwagency.com>
2018-03-09 17:17:04 +0700

    DR: Add style for recent blog

--- src/routes/components/RecentBlog.js
+++ src/routes/components/RecentBlog.js
@@ -27,6 +27,8 @@ class BlogList extends Component {
       itemData.push(
         <div key={index} className="grid-feed__item grid-feed__item--blog" style={{backgroundImage: "url(" + item.field_blog_image[0].url + ")"}}>
           <div className="grid-feed__content">
+            <div className="grid-feed__date">{item.field_date_time[0].value}</div>
+            <h4 className="grid-feed__title">{item.title[0].value}</h4>
           </div>
           <div className="grid-feed__link"><Link to={"blog/" + item.nid[0].value }></Link></div>
        </div>);
